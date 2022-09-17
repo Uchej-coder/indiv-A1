f1c51408b7ddf832b43ad4417416f46b1fad6e9c
Uchej-coder <57462985+Uchej-coder@users.noreply.github.com>
2022-09-16 23:26:33 -0700

    test

--- tester.java
+++ tester.java
@@ -1,3 +1,29 @@
+import java.util.Arrays;
+
 public class tester {
+
+    public static void main (String[] args) {
+
+        insertionTest();
+        selectionTest();
+
+        System.out.println("Both tests have passed successfully");
+        
+    }
+
+    private static void insertionTest() {
+        int [] arr1 = {3, 2, 1};
+        int [] newArray = {1, 2, 3};
+        sorting.insertionSorting(arr1);
+        assert Arrays.equals(arr1, newArray);
+    }
+
+    private static void selectionTest() {
+        int [] arr2 = {3, 1, 2};
+        int [] newArray = {1, 2, 3};
+        sorting.selectSort(arr2);
+        assert Arrays.equals(arr2, newArray);
+    }
+
     
 }
